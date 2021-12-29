ce3929fa7a8f2f5521e1f0da416c79f050b52226
Jahan <34219176+JahanUoL@users.noreply.github.com>
2021-12-29 15:04:05 +0000

    quote list

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,16 @@
+import QuoteForm from '../components/quotes/QuoteForm';
+
 export const NewQuote = (params) => {
+
+    const addQuoteHandler = (quote) => {
+        // add to quote list
+    }
+
+
     return (
         <div>
             <h1>New Quote</h1>
+            <QuoteForm onAddQuote={addQuoteHandler} isLoading={false} />
         </div>
     )
 }
