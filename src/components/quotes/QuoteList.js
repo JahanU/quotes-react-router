7a799595ba64f411aca677e54b2dca1b45a69ebb
Jahan <34219176+JahanUoL@users.noreply.github.com>
2022-01-05 10:04:42 +0000

    sorting list via queryparams

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -2,12 +2,42 @@ import { Fragment } from 'react';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParam = new URLSearchParams(location.search);
+  let sorting = queryParam.get('sort');
+  sorting = sorting === 'asc' ? 'desc' : 'asc';
+
+  console.log(sorting)
+  const sortedQuotes = sortQuotes(props.quotes, sorting === 'asc');
+
+  const changeSortHandler = (event) => {
+    history.push('/quotes?sort=' + sorting);
+  }
+
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}> Sort {sorting.toUpperCase()}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
