a5270ea8c5b95346d6bf6174dac4b78cd343a2ae
Jahan <34219176+JahanUoL@users.noreply.github.com>
2022-01-05 11:47:02 +0000

    get all quotes with usehttp hooks

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,27 +1,45 @@
 import QuoteList from '../components/quotes/QuoteList';
 import { getAllQuotes } from '../lib/api';
 import { useEffect, useState } from 'react';
-
-
-const dummyData = [
-    { id: 1, author: 'Nelson Mandela', quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall' },
-    { id: 2, author: 'John Lennon', quote: `Life is what happens when you're busy making other plans.` },
-    { id: 3, author: 'Walt Disney', quote: 'The way to get started is to quit talking and begin doing' },
-]
+import useHttp from '../hooks/use-http';
+import LoadingSpinner from '../components/UI/LoadingSpinner'
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
 export const AllQuotes = (params) => {
 
-    const [quotes, setQuotes] = useState([]);
+    const { sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+
+    console.log(loadedQuotes);
 
     useEffect(() => {
-        getAllQuotes()
-            .then(quotes => setQuotes(quotes));
-    }, []);
+        sendRequest();
+    }, [sendRequest]);
+
+    if (status === 'pending') {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className='centered'>
+                <h3>Error</h3>
+            </div>
+        )
+    }
+
+    if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+        return <NoQuotesFound />
+    }
 
     return (
         <div>
             <h1>All Quotes</h1>
-            <QuoteList quotes={quotes} />
+            <QuoteList quotes={loadedQuotes} />
+
         </div>
     )
 
